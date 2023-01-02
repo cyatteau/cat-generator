1dc0f45c76bb6bd31e2f87ea524cdf4da80bb18e
cyatteau <112517097+cyatteau@users.noreply.github.com>
2023-01-02 18:12:28 -0500

    Add Images on Load

--- src/AnimalImage.js
+++ src/AnimalImage.js
@@ -1,27 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function AnimalImage() {
   const [theImage, setTheImage] = useState("");
   const url = "https://aws.random.cat/meow?ref=apilist.fun";
+  const [topText, setTopText] = useState("");
+  const [bottomText, setBottomText] = useState("");
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setTheImage(data.file));
+  }, []);
 
   const handleClick = async () => {
     try {
       const res = await fetch(url);
       const json = await res.json();
-      console.log(json.file);
       setTheImage(json.file);
     } catch (e) {
       console.log("Error: ", e);
     }
   };
 
+  function handleChange(event) {
+    setTopText(event.target.value);
+  }
+
+  function handleBottomChange(event) {
+    setBottomText(event.target.value);
+  }
+
   return (
     <div>
       <div className="form">
         <div className="inputs">
+          <input
+            placeholder="Enter top text..."
+            value={topText}
+            onChange={handleChange}
+          />
+          <input
+            placeholder="Enter bottom text..."
+            value={bottomText}
+            onChange={handleBottomChange}
+          />
         </div>
-        {theImage && <img src={theImage} className="catImage" alt="cat"/>}
         <button onClick={handleClick}>Get a new cat 🐱 image</button>
+        <div className="top">{topText}</div>
+        {theImage && <img src={theImage} className="catImage" alt="cat" />}
+        <div className="bottom">{bottomText}</div>
       </div>
     </div>
   );
